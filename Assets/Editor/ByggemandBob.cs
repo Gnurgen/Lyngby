c9473111fdc731b3c967dc10336bbf7490840cf4
Gnurgen <gnurgen@gmail.com>
2016-09-13 14:26:44 +0200

    More command line build setup

---
+++ Assets/Editor/ByggemandBob.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class ByggemandBob{
+
+	static void AndroidBuild(){
+
+		string basePath = "C:/Builds/Android";
+
+		string[] scenes = { "Assets/Scenes/Main.unity" };
+
+		FileUtil.DeleteFileOrDirectory ("C:/Users/Kenned/AppData/LocalUnity/Editor/Editor.log");
+
+		string buildFolder = System.DateTime.Now.ToString ("dd-MM-yy HH.mm.ss");
+
+		Directory.CreateDirectory (basePath + "/" + buildFolder);
+
+		BuildPipeline.BuildPlayer (scenes, basePath + "/" + buildFolder + "/" + "buildfile.apk" , BuildTarget.Android, BuildOptions.None);
+
+		FileUtil.CopyFileOrDirectory ("C:/Users/Kenned/AppData/LocalUnity/Editor/Editor.log", basePath + "/" + buildFolder + "/log.txt");
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+
+	public GameObject pos1;
+	public GameObject pos2;
+	public float speed;
+	GameObject target;
+	// Use this for initialization
+	void Start () {
+		target = pos1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards (transform.position,target.transform.position, speed * Time.deltaTime);
+		if(Vector2.Distance(target.transform.position, transform.position) < 1f){
+			if(target.tag == "pos1"){
+				target = pos2;
+			}
+			else{
+				target = pos1;
+			}
+		}
+	}
+}
